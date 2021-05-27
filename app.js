510669143598d2769dba67c9e00bc9664abf3fbc
zhangjinpei3 <zhangjinpei3@jd.com>
2021-05-27 22:40:05 +0800

    111

--- app.js
+++ app.js
@@ -20,7 +20,18 @@ const { getFileKey, checkDirExist } = require('./util/upload-helper');
 require('./dbhelper/db');
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin() { // 设置允许来自指定域名请求
+      return '*'; // 允许来自所有域名请求
+    },
+    maxAge: 5, // 指定本次预检请求的有效期，单位为秒。
+    credentials: true, // 是否允许发送Cookie
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 设置所允许的HTTP请求方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], // 设置服务器支持的所有头信息字段
+    // exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 设置获取其他自定义字段
+  },
+));
 
 // error handler
 onerror(app);
